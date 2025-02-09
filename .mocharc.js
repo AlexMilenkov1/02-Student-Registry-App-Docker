4782ef25b47023ab19cb7837b40cfaf35e24dd15
Alex Milenkov <alexmilenkov07@gmail.com>
2025-02-09 22:34:55 +0200

    test 2 for jenkins

--- .mocharc.js
+++ .mocharc.js
@@ -1,4 +1,4 @@
-// .mocharc.js - Mocha config file test
+// .mocharc.js - Mocha config file test 2
 const {colors, symbols} = 
   require('mocha/lib/reporters/base');
 // Change the default console UI colors
